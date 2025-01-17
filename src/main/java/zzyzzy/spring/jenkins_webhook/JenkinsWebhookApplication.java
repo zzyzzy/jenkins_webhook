a377c77c048146e851382d2c5f5fc74da47ad385
zzyzzy <zzyzigy@gmail.com>
2025-01-17 10:55:01 +0900

    Initial commit

---
+++ src/main/java/zzyzzy/spring/jenkins_webhook/JenkinsWebhookApplication.java
@@ -0,0 +1,13 @@
+package zzyzzy.spring.jenkins_webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsWebhookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsWebhookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zzyzzy/spring/jenkins_webhook/JenkinsWebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package zzyzzy.spring.jenkins_webhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsWebhookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
